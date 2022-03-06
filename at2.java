7d723251e1213f3748a61e88c4dd482c8770e4d6
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-03-06 13:38:05 +0900

    にこめのこみっと

---
+++ at2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class at2 {
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		double X = sc.nextInt();
+        X=X/10;
+		System.out.println(Math.floor(X) );
+    
+		
+	}
+}
